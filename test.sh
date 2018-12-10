229ee7fe537aca1d242ad0c0961dc0fda902e9f8
Arne Hilmann <arne@hilmann.de>
2018-12-10 12:38:51 +0100

    fix a2s:type problem, adjust to newest roughjs version, add tests

--- test.sh
+++ test.sh
@@ -1,34 +1,39 @@
 #!/bin/bash
 set -e -E -u -o pipefail -C
 
-SOURCE=${1:-examples/logo-sketch.a2s}
-HOST=${2:-http://localhost:22753}
-OUT=${3:-out}
-
-BASENAME=$(basename $SOURCE .a2s)
-SVG0=$OUT/$BASENAME.0.svg
-SVG1=$OUT/$BASENAME.1.svg
-SVG2=$OUT/$BASENAME.2.svg
+HOST=${1:-http://localhost:22753}
+OUT=${2:-out}
 INDEX=$OUT/index.html
 
 rm -rf $OUT/* && mkdir -p $OUT
-curl -s -S --data "$(cat $SOURCE)" -o $SVG0 $HOST/a2svg
-curl -s -S --data "$(cat $SVG0)" -o $SVG1 "$HOST/s2sketch?id=foo&hideFilledPathes=true"
-curl -s -S --data "$(cat $SOURCE)" -o $SVG2 "$HOST/a2sketch?id=bar"
-
 cp vivus*.js *.ttf $OUT/
-
 exec 3>&1 1>$INDEX
 echo "<html><head><style>svg {border: 1pt solid black; vertical-align: top;}</style></head><body>"
-# echo "<pre>$(cat $SOURCE)</pre>" >> $INDEX
-for F in $OUT/*.svg; do
-    cat $F
+
+BASENAMES=()
+for SOURCE in examples/*.a2s; do
+    BASENAME=$(basename $SOURCE .a2s)
+    BASENAMES+=($BASENAME)
+    SVG0=$OUT/$BASENAME.0.svg
+    SVG1=$OUT/$BASENAME.1.svg
+    SVG2=$OUT/$BASENAME.2.svg
+
+    curl -s -S --data "$(cat $SOURCE)" -o $SVG0 $HOST/a2svg
+    curl -s -S --data "$(cat $SOURCE)" -o $SVG1 "$HOST/a2sketch?id=bar"
+    curl -s -S --data "$(cat $SVG0)" -o $SVG2 "$HOST/s2sketch?id=$BASENAME&hideFilledPathes=true"
+
+    for F in $OUT/$BASENAME*.svg; do
+        cat $F
+    done
+    echo "</br>"
 done
+
 echo "<script src='vivus.min.js'></script>"
 echo "<script src='vivus-helper.js'></script>"
-echo "<script>new Vivus('foo', {type: 'oneByOne', duration: 200}, displayAll(200));</script>"
+for BASENAME in ${BASENAMES[@]}; do
+    echo "<script>new Vivus('$BASENAME', {type: 'oneByOne', duration: 200}, displayAll(200));</script>"
+done
 echo "</body></html>"
 exec 1>&3
-
 
 echo "done."
